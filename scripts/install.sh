3360f4822534bd08e9a1db0e008dd25b5f404fab
My Name <xxx@yyy.zzz>
2021-10-10 20:15:51 +0300

    finnaly

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 sudo apt update
-sudo apt install -y apache2
+sudo apt install -y apache2 python3 python3-pip libapache2-mod-wsgi-py3
+sudo pip3 install virtualenv
+virtualenv venv
+source venv/bin/activate
+sudo pip3 install -r requirements.txt
 
 mydir=${PWD##*/}
 
